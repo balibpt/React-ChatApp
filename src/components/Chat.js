1cf843ab0d715ec9c9e3b0b5923abffeced9859a
Bali Tok <bali.tok@hotmail.com>
2023-04-25 15:18:42 +0800

    Update Chat.js

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import {
   collection,
   addDoc,
@@ -18,6 +18,8 @@ function Chat(props) {
 
   const messageRef = collection(db, "messages");
 
+  const chatBoxRef = useRef(null); // create a reference to the chat box element
+
   useEffect(() => {
     let queryMessages = query(
       messageRef,
@@ -30,10 +32,16 @@ function Chat(props) {
         messages.push({ ...doc.data(), id: doc.id });
       });
       setAllMessages(messages);
+
+      // scroll to the bottom of the chat box element
+      chatBoxRef.current.scrollTo({
+        top: chatBoxRef.current.scrollHeight,
+        behavior: "smooth",
+      });
     });
 
     return () => unsubscribe();
-  }, []);
+  }, []); // add room to the dependency array to re-run the effect when the room changes
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -45,38 +53,61 @@ function Chat(props) {
         user: auth.currentUser.displayName,
         room,
       });
+
+      // clear the message input field
+      setMessage("");
+
+      // scroll to the bottom of the chat box element
+      chatBoxRef.current.scrollTo({
+        top: chatBoxRef.current.scrollHeight,
+        behavior: "smooth",
+      });
     } catch (error) {
       console.error(error);
     }
-
-    setMessage("");
   };
 
   return (
     <React.Fragment>
-      <div>
-        <h1>Room: {room}</h1>
-        <div>
-          {allMessages.map((message) => (
-            <div key={message.id}>
-              <h1>
-                <span>{message.user}: </span>
-                {message.text}
-              </h1>
+      <div className="h-screen flex justify-center items-center">
+        <div className="border rounded-xl shadow-md overflow-hidden">
+          <div className="border-b text-center text-4xl bg-blue-300 px-64 py-6">
+            Room Name: {room}
+          </div>
+          <div
+            ref={chatBoxRef} // set the chat box element as the reference
+            className="px-4 py-4 h-64 overflow-scroll"
+          >
+            {allMessages.map((message) => (
+              <div key={message.id} className="mb-4">
+                <h1 className="font-bold">
+                  {message.user}:{" "}
+                  <span className="text-gray-500 font-medium">
+                    {message.text}
+                  </span>{" "}
+                </h1>
+              </div>
+            ))}
+          </div>
+          <form onSubmit={handleSubmit} className="border-t border-gray-600">
+            <div className="w-full flex justify-between items-center h-14">
+              <input
+                type="text"
+                placeholder="Enter Message"
+                onChange={(e) => {
+                  setMessage(e.target.value);
+                }}
+                value={message}
+                className="border-none mx-4 my-2 px-4 py-2 mr-2 focus:border-none focus:ring-0 focus:outline-none"
+              />
+              <div className="border-l h-full flex items-center justify-center hover:bg-blue-500">
+                <button type="submit" className="px-4">
+                  Send
+                </button>
+              </div>
             </div>
-          ))}
+          </form>
         </div>
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            placeholder="Enter Message"
-            onChange={(e) => {
-              setMessage(e.target.value);
-            }}
-            value={message}
-          />
-          <button type="submit">Send</button>
-        </form>
       </div>
     </React.Fragment>
   );
