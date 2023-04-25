b23ead0a95ec393a986c92b77626f43dfb33d06e
Bali Tok <bali.tok@hotmail.com>
2023-04-25 14:36:24 +0800

    added styling to room form

--- src/App.js
+++ src/App.js
@@ -41,21 +41,46 @@ function App() {
             <Chat room={room} />
           </div>
         ) : (
-          <div>
-            <h1>Enter Room Name</h1>
-            <label>Room Name:</label>
-            <input type="text" ref={roomRef} />
+          <div className=" flex flex-col justify-center items-center h-screen">
+            <div class="w-full mx-auto max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
+              <div class="space-y-6">
+                <h5 class="text-xl font-medium text-gray-900 dark:text-white text-center">
+                  Enter Room Name
+                </h5>
+                <div>
+                  <label
+                    for="email"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white text-center"
+                  >
+                    Room Name
+                  </label>
+                  <input
+                    type="text"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                    placeholder="Enter Room Name"
+                    ref={roomRef}
+                    required
+                  />
+                </div>
+                <button
+                  type="submit"
+                  class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  onClick={(e) => {
+                    setRoom(roomRef.current.value);
+                  }}
+                >
+                  Enter
+                </button>
+              </div>
+            </div>
             <button
-              onClick={() => {
-                setRoom(roomRef.current.value);
-              }}
+              className="mt-4 border py-1 px-10 text-center rounded-xl bg-gray-300 hover:bg-gray-500"
+              onClick={handleSignOut}
             >
-              Enter Chat Room
+              Sign Out
             </button>
           </div>
         )}
-
-        <button onClick={handleSignOut}>Sign Out</button>
       </div>
     </React.Fragment>
   );
